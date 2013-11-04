9f269a789bcc6bf2567f6bb3691c2c97c06cb398
umlcat <mail.umlcat@gmail.com>
2013-11-04 10:03:10 -0600

    Create anylibrary.php
    EXAMPLE of the "system.php" crossplatform path library.

---
+++ anylibrary.php
@@ -0,0 +1,33 @@
+<?php
+// ======================================================================
+// Filename: "anylibrary.php"
+// Description:
+// EXAMPLE of the "system.php" crossplatform path library.
+// ======================================================================
+
+/* ## void */ function anylibrary_DoSomething()
+{    
+	// ...
+} // ## void function anylibrary_DoSomething(...)
+
+/* ## void */ function anylibrary_DoFoo()
+{    
+	// ...
+} // ## void function anylibrary_DoFoo(...)
+
+/* ## void */ function anylibrary_DoBar()
+{    
+	// ...
+} // ## void function anylibrary_DoBar(...)
+
+/* ## void */ function anylibrary_CanIHasCheeseBurguer()
+{    
+	// ...
+} // ## void function anylibrary_CanIHasCheeseBurguer(...)
+
+/* ## void */ function anylibrary_CanIHasFishBurguer()
+{    
+	// ...
+} // ## void function anylibrary_CanIHasFishBurguer(...)
+
+?>
